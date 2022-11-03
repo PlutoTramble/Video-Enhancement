63ec96d3f3c93f484cbeebedf17f90b64a71d1d5
PlutoTramble <ced.tramble@pm.me>
2022-11-02 20:09:45 -0400

    INIT Added .gtignore, and just starting cleaning up code

--- main.py
+++ main.py
@@ -8,23 +8,25 @@ import MediaHandler
 
 current_dir = os.getcwd()
 
-def getOpt(argv):
 
-    options = {"input":"",
-               "output":"", 
-               "temporaryDirectoryLocation":"", 
-               "isInputAFile":True, 
-               "isOutputAFile":True, 
-               "targetFPS":60, 
-               "resolutionThreshold":"720x480"
-               } 
+def getOpt(argv):
+    options = {"input": "",
+               "output": "",
+               "temporaryDirectoryLocation": "",
+               "isInputAFile": True,
+               "isOutputAFile": True,
+               "targetFPS": 60,
+               "resolutionThreshold": "720x480"
+               }
 
     try:
-        opts, args = getopt.getopt(argv,"hi:o:t:f:r:", \
-            ["input-file/folder=", "output-file/folder=", 
-            "tmp-location=", "fps=", "resolution-threshold="])
+        opts, args = getopt.getopt(argv, "hi:o:t:f:r:",
+                                   ["input-file/folder=",
+                                    "output-file/folder=",
+                                    "tmp-location=", "fps=",
+                                    "resolution-threshold="])
     except getopt.getopt.GetoptError:
-        print ("main.py -i <input-file/folder> -o <output-file/folder>")
+        print("main.py -i <input-file/folder> -o <output-file/folder>")
         sys.exit(2)
 
     for option, argument in opts:
@@ -32,13 +34,13 @@ def getOpt(argv):
         if option == '-h':
             print("\nmain.py -i <input-file/folder> -o <output-file/folder>\n")
             print("-h to show argument help")
-            print("-t to indicate where you want to create "\
-                "your temporary directory (To reduce wear on your storage)")
-            print("-f to target a specific framerate. "\
-                "Defaults at 60 and you can't go more than 180.")
-            print("-r if the specified resolution is under "\
-                "what you wrote in this format : \"<number>x<number>\" "\
-                "it will double the resolution. Defaults at 720x480")
+            print("-t to indicate where you want to create "
+                  "your temporary directory (To reduce wear on your storage)")
+            print("-f to target a specific framerate. "
+                  "Defaults at 60 and you can't go more than 180.")
+            print("-r if the specified resolution is under " 
+                  "what you wrote in this format : \"<number>x<number>\" "
+                  "it will double the resolution. Defaults at 720x480")
             sys.exit()
 
         # Argument for input
@@ -61,11 +63,11 @@ def getOpt(argv):
                 path = argument
             options["output"] = path
 
-            if os.path.exists(path): #if not, assume it will output a file
+            if os.path.exists(path):  # if not, assume it will output a file
                 if os.path.isfile(path):
                     print("The file already exist for the output argument.")
-                    response = input("Do you wish to overwrite it? "\
-                        "\nIt will delete immediately. y/n ")
+                    response = input("Do you wish to overwrite it? " \
+                                     "\nIt will delete immediately. y/n ")
                     if response[0].lower() == 'y':
                         os.remove(path)
                     else:
@@ -73,7 +75,7 @@ def getOpt(argv):
                         sys.exit()
                 else:
                     options["isOutputAFile"] = False
-        
+
         # Argument for temporary directory location
         elif option in ("-t", "--tmp-location"):
             path = f"{current_dir}/{argument}"
@@ -83,11 +85,11 @@ def getOpt(argv):
 
             if os.path.exists(path):
                 if os.path.isfile(path):
-                    raise IOError("The path specified for "\
-                        "the temporary directory is a file.")
+                    raise IOError("The path specified for " \
+                                  "the temporary directory is a file.")
             else:
-                raise IOError("The path specified for the "\
-                    "temporary directory doesn't exist.")
+                raise IOError("The path specified for the " \
+                              "temporary directory doesn't exist.")
 
         # Argument for frames per second
         elif option in ("-f", "--fps"):
@@ -102,11 +104,12 @@ def getOpt(argv):
     if options['temporaryDirectoryLocation'] == "":
         options["temporaryDirectoryLocation"] = current_dir
 
-    if options["isInputAFile"] == False and\
+    if options["isInputAFile"] == False and \
             options["isOutputAFile"] == True:
         raise IOError("It's impossible to take a whole directory into a file.")
 
     return options
+
 
 def makeTempDir():
     print(f"Making temporary directory in : {tmpDirectory}")
@@ -120,7 +123,6 @@ def makeTempDir():
     os.mkdir(f"{tmpDirectory}/vidout")
 
 
-
 if __name__ == "__main__":
     options = getOpt(sys.argv[1:])
     tmpDirectory = f'{options["temporaryDirectoryLocation"]}/ave-tmp'
@@ -128,26 +130,25 @@ if __name__ == "__main__":
     # Checking if the "AIs" folder and its content exists"
     if not os.path.exists(f"{current_dir}/AIs"):
         os.mkdir(f"{current_dir}/AIs")
-        raise FileNotFoundError("The \"AIs\" directory didn't exist. "\
-            "The program created it but you need to put stuff in it. "\
-            "Follow the instructions on the repository.")
-    
+        raise FileNotFoundError("The \"AIs\" directory didn't exist. " \
+                                "The program created it but you need to put stuff in it. " \
+                                "Follow the instructions on the repository.")
+
     if not os.path.exists(f"{current_dir}/AIs/rife-ncnn-vulkan") or \
             not os.path.exists(f"{current_dir}/AIs/rife-v2.3") or \
             not os.path.exists(f"{current_dir}/AIs/ifrnet-ncnn-vulkan") or \
             not os.path.exists(f"{current_dir}/AIs/IFRNet_L_Vimeo90K") or \
             not os.path.exists(f"{current_dir}/AIs/srmd-ncnn-vulkan") or \
             not os.path.exists(f"{current_dir}/AIs/models-srmd"):
-        raise FileNotFoundError("There are file(s) that are missing."\
-            " Please go look Where to put stuff in the repository.")
+        raise FileNotFoundError("There are file(s) that are missing." \
+                                " Please go look Where to put stuff in the repository.")
 
     # Checking if user have ffmpeg
     try:
         subprocess.call(["ffmpeg"], shell=False, \
-            stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+                        stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     except OSError("You need to install ffmpeg before running this program."):
         sys.exit(2)
-    
 
     ## Where the handling happens
     if options["isInputAFile"]:
